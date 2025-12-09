861c5dfef7bae08dd3dc9cafb7b13b7461750d37
fraz <fraz@o2.pl>
2025-12-09 15:07:57 +0100

    disable core auto-updates

--- functions.php
+++ functions.php
@@ -62,7 +62,7 @@ add_action( 'admin_init', __NAMESPACE__ . '\add_google_maps_settings' );
 /**
  * Security.
  */
-add_filter( 'auto_update_core', '__return_true' );
+add_filter( 'auto_update_core', '__return_false' );
 add_filter( 'auto_update_plugin', '__return_true' );
 add_filter( 'auto_update_theme', '__return_true' );
 add_filter( 'auto_update_translation', '__return_true' );
