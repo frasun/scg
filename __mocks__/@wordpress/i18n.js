d481e546011fc998db36016458d95b2a75d25731
fraz <fraz@o2.pl>
2026-01-02 09:01:46 +0100

    move jest mocks to package

--- __mocks__/@wordpress/i18n.js
+++
@@ -1,1 +0,0 @@
-export const __ = ( text ) => text;
--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,16 @@ module.exports = {
 	...config,
 	setupFilesAfterEnv: [ '<rootDir>/tests/js/jest.setup.js' ],
 	testMatch: [ '<rootDir>/tests/js/*.test.js' ],
+	transformIgnorePatterns: [ 'node_modules/(?!(wordpress-jest-mocks)/)' ],
+	moduleNameMapper: {
+		...config.moduleNameMapper,
+		'^@wordpress/components$':
+			'<rootDir>/node_modules/wordpress-jest-mocks/components.js',
+		'^@wordpress/block-editor$':
+			'<rootDir>/node_modules/wordpress-jest-mocks/block-editor.js',
+		'^@wordpress/interactivity$':
+			'<rootDir>/node_modules/wordpress-jest-mocks/interactivity.js',
+		'^@wordpress/i18n$':
+			'<rootDir>/node_modules/wordpress-jest-mocks/i18n.js',
+	},
 };
